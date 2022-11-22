24468983d0a729a3bb1897dc9081b7f5199a2cc9
NatalyaZinger <nata.zinger@gmail.com>
2022-11-22 21:42:10 +0500

    pages added

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -1,4 +1,35 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
+
 public class LoginPage {
+    SelenideElement login = $("[data-test-id='login'] input");
+    SelenideElement password = $("[data-test-id='password'] input");
+    SelenideElement loginButton = $("[data-test-id='action-login']");
+    SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification__content");
+    SelenideElement errorEmptyLogin = $("[data-test-id='login'] .input__sub");
+    SelenideElement errorEmptyPass = $("[data-test-id='password'] .input__sub");
+
+
+    public void verifyErrorNotificationVisibility() {
+        errorNotification.shouldBe(visible);
+    }
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        login.setValue(info.getLogin());
+        password.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+        //return page(VerificationPage.class);
+    }
+
 }
+
+
+
--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -1,4 +1,29 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
 public class VerificationPage {
+    SelenideElement codeInput = $("[data-test-id='code'] input");
+    SelenideElement verifyButton = $("[data-test-id='action-verify']");
+    SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification__content");
+    //SelenideElement errorEmptyCode = $("[data-test-id='code'] .input__sub");
+    //SelenideElement header = $(".paragraph");
+
+    public void verifyVerificationPageVisibility() { codeInput.shouldBe(visible); }
+
+    public void verifyErrorNotificationVisibility() { errorNotification.shouldBe(visible); }
+
+    public void verify(String verificationCode) {
+        codeInput.setValue(verificationCode);
+        verifyButton.click();
+    }
+
+    public DashboardPage validVerify(String verificationCode) {
+        verify(verificationCode);
+        return new DashboardPage();
+    }
 }
